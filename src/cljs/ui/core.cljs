f75d0f2069042ff61e523ac85750420ccdc2ba56
Emil Bengtsson <cn3241@bdo.no>
2018-03-19 14:27:25 +0100

    connect with REPL

--- src/cljs/ui/core.cljs
+++ src/cljs/ui/core.cljs
@@ -1,5 +1,6 @@
 (ns ui.core
   (:require [reagent.core :as reagent]
+            [weasel.repl :as repl]
             [ui.config :as config]
             [ui.routes :as routes]
             [ui.views :as views]
@@ -13,7 +14,9 @@
 (defn- dev-setup []
   (when config/debug?
     (enable-console-print!)
-    (util/log "dev mode")))
+    (util/log "dev mode")
+    (util/log "Setting up REPL")
+    (repl/connect "ws://localhost:9001")))
 
 (defn- mount-root []
   (reagent/render [views/main-panel]
